db416b0c040a5a1c1c7b856a4c64080187a45af2
James Scott-Brown <james@jamesscottbrown.com>
2017-03-22 14:57:51 +0000

    On project page, update displayed 'Saved specification' on save

--- vse/static/js/dragrect.js
+++ vse/static/js/dragrect.js
@@ -462,22 +462,23 @@ function setup(div_name, spec_id, index, options) {
     d3.select(div_name).append('button')
         .text("Save")
         .on("click", function(){
+            var new_spec_string = getSpecString();
             $.ajax({
             type: "POST",
             contentType: "application/json; charset=utf-8",
             url: "http://" + window.location.host + "/specifications/" + spec_id + "/save",
-            dataType: 'json',
+            dataType: 'html',
             async: true,
-            data: getSpecString(),
+            data: new_spec_string,
 
              beforeSend: function(xhr, settings) {
                 xhr.setRequestHeader("X-CSRFToken", csrf_token);
              },
 
             success: function (data) {
-               console.log("Saved");
+                d3.select("#spec_string_" + spec_id).text(new_spec_string)
             },
-            error: function (result) {}
+            error: function (result, textStatus) { }
             })
         });
 
